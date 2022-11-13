ba87a8c688d276aa6e21bae7a769dc63ee8d6623
feed-mina <myelin24@naver.com>
2022-11-13 21:32:02 +0900

    스프링 config / DI 주석 ë

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -18,4 +18,7 @@ public class SpringConfig {
     public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
+
+    // @Autowired를 통한 DI는 helloController , MemberService 등과 같이 스프링이 관리하는 객체에서만 동작한다. 스프링 빈으로 등로가힞 않고 내가 직접 생성한 객체에서는 동작하지 않는다.
+
 }
--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -9,6 +9,9 @@ public class MemberController {
 
     private final MemberService memberService;
 
+     // DI에는 필드 주입 , setter 주입, 생성자 주입 3가지 방법이 있다.
+     // 생성자를 통해 주입 -> 애플리케이션 조립 : 이후 setter처럼 작동 , setter injection은 아무 개발자나 가능할 수 있다.
+    // 조립 시점에 주입하면 이후 변경할 일이 없기 때문에 Controller는 생성자 조립을 권장함 정형화 되지 않거나 상황에 따라 구현 클래스를 변경해ㅑㅇ 하면 설정을 통해 스프링 빈을 설정한다.
     @Autowired
     public MemberController(MemberService memberService) {
 
