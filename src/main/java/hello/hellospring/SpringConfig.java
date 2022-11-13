ad6ca036ac43aac898a8d8159715b99adace6997
feed-mina <myelin24@naver.com>
2022-11-13 23:01:52 +0900

    회통합 테스트 환경 구축하기

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,14 +1,26 @@
 package hello.hellospring;
 
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -16,7 +28,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+
+        return new JdbcMemberRepository(dataSource);
     }
 
     // @Autowired를 통한 DI는 helloController , MemberService 등과 같이 스프링이 관리하는 객체에서만 동작한다. 스프링 빈으로 등로가힞 않고 내가 직접 생성한 객체에서는 동작하지 않는다.
