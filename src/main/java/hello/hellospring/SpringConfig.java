83fc7777c041d60ddc66035a2a46326ced2b5248
feed-mina <myelin24@naver.com>
2022-11-15 21:35:18 +0900

    Jdbc Template Repository 적용

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -2,6 +2,7 @@ package hello.hellospring;
 
 
 import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
@@ -28,8 +29,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-
-        return new JdbcMemberRepository(dataSource);
+        // return new MemoryMemberRepository();
+        // return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 
     // @Autowired를 통한 DI는 helloController , MemberService 등과 같이 스프링이 관리하는 객체에서만 동작한다. 스프링 빈으로 등로가힞 않고 내가 직접 생성한 객체에서는 동작하지 않는다.
